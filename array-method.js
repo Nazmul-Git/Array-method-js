cfda1baef9242d14d37319c5cae27b5a9f8f7666
Nazmul-Git <130184909+Nazmul-Git@users.noreply.github.com>
2023-11-09 22:13:35 +0600

    Reverse() array method.

--- array-method.js
+++ array-method.js
@@ -28,3 +28,11 @@ console.log(newArr.splice(1,3)); //[ 'x', 'y', 'z' ]
 
 
 
+// Reverse
+arr=['a','b','c','d','e'];
+const arr2=['j','i','h','g','f'];
+console.log(arr2.reverse()); //[ 'f', 'g', 'h', 'i', 'j' ]
+// console.log(arr2); //[ 'f', 'g', 'h', 'i', 'j' ]
+
+
+
