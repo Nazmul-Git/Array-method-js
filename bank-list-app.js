99b2b8579d88075bdd8b76a8509055bfe4db82bc
Nazmul-Git <130184909+Nazmul-Git@users.noreply.github.com>
2023-11-10 16:32:18 +0600

    Array method forEach().

--- bank-list-app.js
+++ bank-list-app.js
@@ -58,11 +58,7 @@ const account1 = {
   /////////////////////////////////////////////////
   // LECTURES
   
-  const currencies = new Map([
-    ['USD', 'United States dollar'],
-    ['EUR', 'Euro'],
-    ['GBP', 'Pound sterling'],
-  ]);
+ 
   
   const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
   
@@ -71,27 +67,27 @@ const account1 = {
 // for of
 for(const move of movements){
     if(move<0){
-        console.log(`You withdrew ${Math.abs(move)}`);
+        // console.log(`You withdrew ${Math.abs(move)}`);
        }else{
-        console.log(`You deposit ${Math.abs(move)}`);
+        // console.log(`You deposit ${Math.abs(move)}`);
        }
 }
 
 //   forEach()
 movements.forEach(function(move){
    if(move<0){
-    console.log(`You withdrew ${Math.abs(move)}`);
+    // console.log(`You withdrew ${Math.abs(move)}`);
    }else{
-    console.log(`You deposit ${Math.abs(move)}`);
+    // console.log(`You deposit ${Math.abs(move)}`);
    }
 });
 
 //using arrow function
 movements.forEach((move)=>{
     if(move<0){
-        console.log(`You withdrew ${Math.abs(move)}`);
+        // console.log(`You withdrew ${Math.abs(move)}`);
        }else{
-        console.log(`You deposit ${Math.abs(move)}`);
+        // console.log(`You deposit ${Math.abs(move)}`);
        }
 });
 
@@ -109,18 +105,45 @@ movements.forEach((move)=>{
 //  use entries in for of loop we can get index number also
 for(const [idx,move] of movements.entries()){
     if(move<0){
-        console.log(`Movement ${idx+1}: You withdrew ${Math.abs(move)}`);
+        // console.log(`Movement ${idx+1}: You withdrew ${Math.abs(move)}`);
        }else{
-        console.log(`Movement ${idx+1}: You deposit ${Math.abs(move)}`);
+        // console.log(`Movement ${idx+1}: You deposit ${Math.abs(move)}`);
        }
 };
 
-// forEach take 3 parameter. forEach(mov, i, arr)
+// forEach take 3 parameter each value, index, array. forEach(mov, i, arr)
 movements.forEach(function(move,idx,arr){
     if(move<0){
-     console.log(`Movement ${idx}: You withdrew ${Math.abs(move)}`);
+    //  console.log(`Movement ${idx}: You withdrew ${Math.abs(move)}`);
     }else{
-     console.log(`Movement ${idx}: You deposit ${Math.abs(move)}`);
+    //  console.log(`Movement ${idx}: You deposit ${Math.abs(move)}`);
     }
-    console.log(arr);
+    // console.log(arr);
  });
+
+
+
+ ////////////////////////////////////////////////////////////
+ const currencies = new Map([
+  ['USD', 'United States dollar'],
+  ['EUR', 'Euro'],
+  ['GBP', 'Pound sterling'],
+]);
+currencies.forEach(function(value,key,map){
+  console.log(`${key}: ${value}`);
+});
+
+// Set
+const currenciesUnique=new Set(['USD', 'USD', 'GDP','EUR','EUR','USD']);
+
+console.log(currenciesUnique);// {'USD', 'GDP', 'EUR'}
+currenciesUnique.forEach(function(value,key,map){
+
+  console.log(`${key} : ${value}`);
+  /** OUTPUT: here key is exactly the same as the value.
+   * USD : USD
+     GDP : GDP
+     EUR : EUR
+   */
+})
+
