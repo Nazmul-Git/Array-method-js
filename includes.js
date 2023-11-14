550b7113c3d1cd62a68098c42ba22557a1ee241e
Nazmul-Git <130184909+Nazmul-Git@users.noreply.github.com>
2023-11-14 23:36:02 +0600

    Add every method when have condition and check by conditions when all element is true/false at a time.

--- includes.js
+++ includes.js
@@ -2,6 +2,11 @@
 const movements = [5000, 3400, -150, -790, -3210, -1000, 8500, -30];
 console.log(movements.includes(-150)); //true
 
-//SOME: CONDITION
+//SOME: CONDITION 
 console.log(movements.some(mov=>mov === -150)); //true
 
+// EVERY :- check all value by condition, when all is true/false then it return true/false.
+console.log(movements.every(mov=>mov > 0)); //false
+
+const movement = [5000, 3400, 150, 790, 3210, 1000, 8500, 30];
+console.log(movement.every(mov=>mov > 0));  //true
