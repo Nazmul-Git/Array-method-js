cedce1f4f2d235aebcfa364d28489dd5e6fc30d7
Nazmul-Git <130184909+Nazmul-Git@users.noreply.github.com>
2023-11-14 23:25:59 +0600

    Add some method when have condition.

--- includes.js
+++ includes.js
@@ -1,2 +1,7 @@
+// EQUALITY
 const movements = [5000, 3400, -150, -790, -3210, -1000, 8500, -30];
 console.log(movements.includes(-150)); //true
+
+//SOME: CONDITION
+console.log(movements.some(mov=>mov === -150)); //true
+
